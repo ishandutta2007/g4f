67eddb8a60b01859546de088dbcea416fdb87e1d
G4F <15249031-g4ff@users.noreply.gitlab.com>
2023-07-27 11:04:34 +0800

    update

--- g4f/Provider/Providers/Ails.py
+++ g4f/Provider/Providers/Ails.py
@@ -82,11 +82,8 @@ def _create_completion(model: str, messages: list, stream: bool = False, **kwarg
     response = requests.post('https://api.caipacity.com/v1/chat/completions?full=false', 
                              headers=headers, data=json_data, stream=True)
     for token in response.iter_lines():
-        if b'content' in token:
-            completion_chunk = json.loads(token.decode().replace('data: ', ''))
-            token = completion_chunk['choices'][0]['delta'].get('content')
-            if token != None:
-                yield token
+        if token != None:
+            yield token.decode()
 
 params = f'g4f.Providers.{os.path.basename(__file__)[:-3]} supports: ' + \
     '(%s)' % ', '.join([f"{name}: {get_type_hints(_create_completion)[name].__name__}" for name in _create_completion.__code__.co_varnames[:_create_completion.__code__.co_argcount]])
