7468e887a194409934d918f89e87189eb9861c76
G4F <15249031-g4ff@users.noreply.gitlab.com>
2023-07-31 21:45:29 +0800

    update

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import g4f
 
 stream = True
 response = g4f.ChatCompletion.create(model='claude-instant-100k', provider=g4f.Provider.B88, messages=[
-                                     {"role": "user", "content": "write a poem in english about apples"}], stream=stream)
+                                     {"role": "user", "content": "hi"}], stream=stream)
 
 if stream:
     for message in response:
