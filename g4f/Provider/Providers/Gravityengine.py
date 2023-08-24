2ca25c3a1bf758752ced55ba4ca613d880756d0f
G4F <15249031-g4ff@users.noreply.gitlab.com>
2023-08-24 20:44:59 +0800

    update

--- g4f/Provider/Providers/Gravityengine.py
+++ g4f/Provider/Providers/Gravityengine.py
@@ -1,6 +1,5 @@
 import requests
 import os
-import json
 from ...typing import sha256, Dict, get_type_hints
 
 url = 'https://gpt4.xunika.uk/'
@@ -12,8 +11,8 @@ working = True
 def _create_completion(model: str, messages: list, stream: bool, **kwargs):
     headers = {
         'Content-Type': 'application/json',
-        'Authorization':'Bearer wwwchatgptorguk',
-        'Cookie':'FCNEC=%5B%5B%22AKsRol8oAtMdGFPKct6Xlvf9FMwt0ghzonq-NWJaGWZgyBfewG7IzKBSKZRpUeq_dOeMsER8VrYaIKOQgwwXT7zOzBtXf_OU7rD44yIjAl03Q4HRRtdsryzUPl2DuQZ8Wq6IMzD9RbCKxYuSCMqEJcDB51IiIZrqeg%3D%3D%22%5D%2Cnull%2C%5B%5D%5D',
+        'Authorization':'Bearer nk-wwwchatgptorguk',
+        'Cookie':'FCNEC=%5B%5B%22AKsRol8oAtMdGFPKct6Xlvf9FMwt0ghzonq-NWJaGWZgyBfewG7IzKBSKZRpUeq_dOeMsER8VrYaIKOQgwwXT7zOzBtXf_OU7rD44yIjAl03Q4HRRtdsryzUPl2DuQZ8Wq6IMzD9RbCKxYuSCMqEJcDB51IiIZrqeg%3D%3D%22%5D%2Cnull%2C%5B%5D%5D; cf_clearance=Of77ONOU17lrhK7iE7EoOLEmvF0f0oansv82kv5WZ8w-1692877407-0-1-e5dbe702.bf00254.b2dab8e2-0.2.1692877407',
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
         'Origin':'https://gpt4.xunika.uk',
         'Referer':'https://gpt4.xunika.uk/'
@@ -22,9 +21,11 @@ def _create_completion(model: str, messages: list, stream: bool, **kwargs):
         'model': model,
         'temperature': kwargs.get("temperature", 0.7),
         'presence_penalty': kwargs.get('presence_penalty', 0),
+        'top_p':1,
+        'frequency_penalty': 0,
         'messages': messages,
     }
-    response = requests.post(url + '/api/openai/v1/chat/completions',
+    response = requests.post('https://gpt4.xunika.uk/api/openai/v1/chat/completions',
                              json=data, headers=headers,stream=True)
     
     yield response.json()['choices'][0]['message']['content']
